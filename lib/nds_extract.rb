cf62dadd8fe8da5b74074ffcad2aec2fb8b8bac4
Nathan Brickett <natey37@gmail.com>
2019-12-23 05:57:57 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
-
+require 'pry'
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 
@@ -48,6 +48,12 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  movies = []
+  movies_collection.each do |movie|
+    updated_movie = movie_with_director_name(name, movie)
+    movies << updated_movie
+  end 
+    movies
 end
 
 
@@ -63,7 +69,17 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-end
+  
+  movies = {}
+  collection.each do |movie|
+    if !movies[movie[:studio]]
+        movies[movie[:studio]] = movie[:worldwide_gross]
+    else 
+        movies[movie[:studio]] += movie[:worldwide_gross]
+    end 
+  end 
+  movies 
+end 
 
 def movies_with_directors_set(source)
   # GOAL: For each director, find their :movies Array and stick it in a new Array
@@ -76,6 +92,18 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+
+  
+  index = 0 
+  array_of_movies = []
+    while index < source.count do 
+      director_info = source[index]
+      director_name = director_info[:name]
+      directors_movies = director_info[:movies]
+      array_of_movies << movies_with_director_key(director_name, directors_movies)
+      index += 1 
+    end 
+  array_of_movies
 end
 
 # ----------------    End of Your Code Region --------------------
@@ -87,3 +115,4 @@ def studios_totals(nds)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
   return gross_per_studio(movies_with_director_names)
 end
+
